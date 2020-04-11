494d74a129c27bef5b3ab36963d6a437e612b73a
DLar <d_larson25@yahoo.com>
2020-04-11 15:43:11 -0500

    Separated definitions by brand

---
+++ js/lib.js
@@ -0,0 +1,104 @@
+const lib = {
+	creative: {
+		1: 'David',
+		2: 'Anthony',
+		3: 'Kelsey',
+		4: 'Ronni'
+	},
+	cutlines: {
+		1: 'LIVE',
+		2: 'DOWN'
+	},
+	states: {
+		1: 'Turn On',
+		2: 'Turn Off',
+		3: 'Removed',
+		4: 'Default',
+		5: 'Coming Soon'
+	},
+	nm: {
+		domains: ['https://wn.ref1.nmg/', 'https://www.neimanmarcus.com/'],
+		tags: ['[NM Approval]', '[NM Schedule]'],
+		types: {
+			1: 'Header Promos (AEM)',
+			2: 'Intl Header Promos (AEM)',
+			3: 'Homepage (AEM)',
+			4: 'Intl Homepage (AEM)',
+			5: 'Drawer Tickers (AEM)',
+			6: 'Designer Indexes (AEM)',
+			7: 'Silo Graphic Headers (AEM)',
+			8: 'Landing Pages (AEM)',
+			9: 'Entry Pages (AEM)',
+			10: 'Graphic Headers (AEM/DAM)',
+			11: 'Silo Banners (DAM)',
+			12: 'Promo Tiles (DAM)',
+			13: 'Popups (ATG)',
+			14: 'Videos (ATG)',
+			15: 'Checkout (ATG)',
+			16: 'PDP Banners (DAM)'
+		}
+	},
+	lp: {
+		domains: ['https://www.neimanmarcus.com/'],
+		tags: ['[LB/MAG Approval]'],
+		types: {
+			1: 'Lookbook (AEM)',
+			2: 'Magazine (AEM)',
+			3: 'Graphic Header (DAM)',
+			4: 'Silo Banner (DAM)',
+			5: 'Promo Tile (DAM)'
+		}
+	},
+	hc: {
+		domains: ['https://whref1/', 'https://www.horchow.com/'],
+		tags: ['[HC Approval]', '[HC Schedule]'],
+		types: {
+			1: 'Header Promos',
+			2: 'Homepage',
+			3: 'Drawer Tickers',
+			4: 'Graphic Headers',
+			5: 'Popup',
+			6: 'Search',
+			7: 'Checkout'
+		}
+	},
+	lc: {
+		domains: ['https://lcref1/', 'https://www.lastcall.com/'],
+		tags: ['[LC Approval]', '[LC Schedule]'],
+		types: {
+			1: 'Header Promos',
+			2: 'Promo Banner',
+			3: 'Homepage',
+			4: 'Drawers',
+			5: 'Silos',
+			6: 'Silo Bottom Bars',
+			7: 'Landing Pages',
+			8: 'Promo Tiles',
+			9: 'Popups',
+			10: 'Promo Sticker',
+			11: 'Checkout',
+			12: 'Store Results Banner',
+			13: 'PDP Button'
+		}
+	},
+	app: {
+		domains: ['https://wn.test3.nmg/', 'https://www.neimanmarcus.com/'],
+		tags: ['[APP Approval]', '[APP Schedule]'],
+		types: {
+			1: 'Home Screen',
+			2: 'Stores'
+		}
+	},
+	ntf: {
+		domains: ['https://www.neimanmarcus.com/'],
+		tags: ['[NTF Approval]'],
+		types: {
+			1: 'Image',
+			2: 'PUSH',
+			3: 'PULL<br>[message center title]',
+			4: '[image copy]',
+			5: '[supporting copy]',
+			6: '[button]'
+		}
+	}
+}
