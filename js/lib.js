23e49e95e4e509abc9c0541a7eefd05e9e38e0e4
DLar <d_larson25@yahoo.com>
2021-02-01 00:05:59 -0600

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -14,42 +14,42 @@ const lib = {
 		header: 'Patches are',
 		domains: ['https://wn.ref1.nmg/', 'https://www.neimanmarcus.com/'],
 		types: {
-			1: 'Header Promos (AEM)',
-			2: 'Intl Header Promos (AEM)',
-			3: 'Homepage (AEM)',
-			4: 'Intl Homepage (AEM)',
-			5: 'Drawer Tickers (AEM)',
-			6: 'Designer Indexes (AEM)',
-			7: 'Silo Graphic Headers (AEM)',
-			8: 'Landing Pages (AEM)',
-			9: 'Entry Pages (AEM)',
-			10: 'Graphic Headers (AEM)',
-			11: 'Silo Banners (DAM)',
-			12: 'Promo Tiles (DAM)',
+			1: 'Header Promos (Contentful)',
+			2: 'Intl Header Promos (Contentful)',
+			3: 'Homepage (Contentful)',
+			4: 'Intl Homepage (Contentful)',
+			5: 'Drawer Tickers (Contentful)',
+			6: 'Designer Indexes (Contentful)',
+			7: 'Silo Graphic Headers (Contentful)',
+			8: 'Landing Pages (Contentful)',
+			9: 'Entry Pages (Contentful)',
+			10: 'Graphic Headers (Contentful)',
+			11: 'Silo Banners (Cloudinary)',
+			12: 'Promo Tiles (Cloudinary)',
 			13: 'Popups (ATG)',
 			14: 'Videos (ATG)',
 			15: 'Checkout (ATG)',
-			16: 'PDP Banners (DAM)'
+			16: 'PDP Banners (Contentful)'
 		}
 	},
 	'LB/MAG': {
 		header: 'Lookbook/Magazine is',
 		domains: ['https://www.neimanmarcus.com/'],
 		types: {
-			1: 'Lookbook (AEM)',
-			2: 'Magazine (AEM)',
-			3: 'Graphic Header (DAM)',
-			4: 'Silo Banner (DAM)',
-			5: 'Promo Tile (DAM)'
+			1: 'Lookbook (Contentful)',
+			2: 'Magazine (Contentful)',
+			3: 'Graphic Header (Cloudinary)',
+			4: 'Silo Banner (Cloudinary)',
+			5: 'Promo Tile (Cloudinary)'
 		}
 	},
 	SS: {
 		header: 'Merchant Site Support is',
 		domains: ['https://author-neimanmarcus-prod.adobecqms.net/mnt/overlay/dam/gui/content/collections.html/content/dam/collections'],
 		types: {
-			1: 'Graphic Header (DAM)',
-			2: 'Silo Banner (DAM)',
-			3: 'Promo Tile (DAM)'
+			1: 'Graphic Header (Cloudinary)',
+			2: 'Silo Banner (Cloudinary)',
+			3: 'Promo Tile (Cloudinary)'
 		}
 	},
 	HC: {
